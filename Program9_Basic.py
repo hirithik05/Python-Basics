b91c67f5ab3099873b626e522ed0cbf33a5e678e
Ritik Kumar Verma <ritikkumarverma05@gmail.com>
2022-09-06 06:17:58 +0530

    List method

---
+++ Program9_Basic.py
@@ -0,0 +1,12 @@
+#List method in Python
+a = [1,2,3,4]
+
+a.insert(3,"Ram")    #First provide index the value
+print(a)
+
+a.append(6)   # add items in last position
+print(a)
+
+a.remove(2)    #delete items by providing element
+print(a)
+
